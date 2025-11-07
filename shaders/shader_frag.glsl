238d1b894ad35a53c85459bbeb1574cd33915d4b
ossi <o.s.e.hage@student.tudelft.nl>
2025-11-07 12:39:50 +0100

    fixed shader

--- shaders/shader_frag.glsl
+++ shaders/shader_frag.glsl
@@ -35,8 +35,8 @@ uniform float lightSpotCosCutoff[MAX_LIGHTS];
 uniform float lightSpotSoftness[MAX_LIGHTS];
 
 uniform bool shadowsEnabled;
-uniform mat4 lightMVP[MAX_LIGHTS];
-uniform sampler2D shadowMap[MAX_LIGHTS];
+uniform mat4 lightMVP;
+uniform sampler2D shadowMap;
 uniform bool pcf;
 
 uniform bool useNormalMap;
@@ -45,51 +45,11 @@ uniform samplerCube environmentMap;
 uniform bool useEnvMap;
 uniform float envMapStrength;
 
-uniform bool useEnvMap;
-uniform samplerCube skybox;
-
 in vec3 fragPosition;
 in vec3 fragNormal;
 in vec2 fragTexCoord;
 
 layout(location = 0) out vec4 fragColor;
-
-float calculateVisibility(int i) {
-    float shadowValue = 1.0;
-    vec4 lightSpacePos = lightMVP[i] * vec4(fragPosition, 1.0);
-    lightSpacePos /= lightSpacePos.w;
-    lightSpacePos = lightSpacePos * 0.5 + 0.5;
-
-    float closestDepth = texture(shadowMap[i], lightSpacePos.xy).r;
-    float currentDepth = lightSpacePos.z;
-    float bias = 0.001;
-
-    if (currentDepth - bias > closestDepth) {
-        shadowValue *= 0.0;
-    }
-
-    if (pcf) {
-        float pixelSize = 1.0 / 1024.0; 
-        shadowValue = 0.0;
-
-        for (int x = -1; x <= 1; ++x) {
-            for (int y = -1; y <= 1; ++y) {
-                vec2 offset = vec2(x, y) * pixelSize;
-
-                float closestDepth = texture(shadowMap[i], lightSpacePos.xy + offset).r;
-
-                if (currentDepth - bias > closestDepth)
-                    shadowValue += 0.0;
-                else
-                    shadowValue += 1.0;
-            }
-        }
-
-        shadowValue /= 9.0;
-
-    }
-    return shadowValue;
-}
 
 const float PI = 3.14159265359;
 
@@ -265,8 +225,7 @@ void main()
 
         vec3 lightContribution = lightColors[i] * spotFactor;
 
-        float visibility = shadowsEnabled ? calculateVisibility(i) : 1.0;
-        colorAccum += visibility * baseColor * diff * lightContribution;
+        colorAccum += baseColor * diff * lightContribution;
 
         if (shadingMode == 2 && diff > 0.0) {
             vec3 reflectDir = reflect(-lightDir, normal);
@@ -288,41 +247,33 @@ void main()
         finalColor += specColor * specAccum;
     }
     
-    if (useEnvMap) {
-        vec3 reflectDir = reflect(-viewDir, normal);
-        vec3 envColor = texture(skybox, reflectDir).rgb;
-        finalColor = envColor;
-    }
-
-<<<<<<< HEAD
-    if (pcf) {
-        float pixelSize = 1.0 / 1024.0; 
-
+    float shadowValue = 1.0;
+    if (shadowsEnabled) {
         vec4 lightSpacePos = lightMVP * vec4(fragPosition, 1.0);
         lightSpacePos /= lightSpacePos.w;
-        lightSpacePos = lightSpacePos * 0.5 + 0.5; 
+        lightSpacePos = lightSpacePos * 0.5 + 0.5;
 
-        float fragLightDepth = lightSpacePos.z;
+        float closestDepth = texture(shadowMap, lightSpacePos.xy).r;
+        float currentDepth = lightSpacePos.z;
         float bias = 0.001;
-        shadowValue = 0.0;
 
-        for (int i = -1; i <= 1; ++i) {
-            for (int j = -1; j <= 1; ++j) {
-                vec2 offset = vec2(i, j) * pixelSize;
+        shadowValue = currentDepth - bias > closestDepth ? 0.0 : 1.0;
 
-                float closestDepth = texture(shadowMap, lightSpacePos.xy + offset).r;
-
-                if (fragLightDepth - bias > closestDepth)
-                    shadowValue += 0.0;
-                else
-                    shadowValue += 1.0;
+        if (pcf) {
+            float pixelSize = 1.0 / 1024.0;
+            float accum = 0.0;
+            for (int i = -1; i <= 1; ++i) {
+                for (int j = -1; j <= 1; ++j) {
+                    vec2 offset = vec2(i, j) * pixelSize;
+                    float sampleDepth = texture(shadowMap, lightSpacePos.xy + offset).r;
+                    accum += (currentDepth - bias > sampleDepth) ? 0.0 : 1.0;
+                }
             }
+            shadowValue = accum / 9.0;
         }
-
-        shadowValue /= 9.0; 
     }
 
-    finalColor = finalColor * shadowValue;
+    finalColor *= shadowValue;
 
     if (useEnvMap && envMapStrength > 0.0) {
         vec3 reflectedDir = reflect(-viewDir, normal);
@@ -332,7 +283,4 @@ void main()
     }
 
     fragColor = vec4(finalColor, 1.0);
-=======
->>>>>>> 5880dd5c9237fdd1f70b48e1f384749c6b424cef
-    fragColor = vec4(finalColor, 1);
 }
